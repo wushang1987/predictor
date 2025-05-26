415da3a9371b0902a39d012126985b0716e94690
Will Wang <wwd1987@outlook.com>
2025-05-26 23:24:46 +0800

    add langchain

---
+++ src/deepseek-langchain.ts
@@ -0,0 +1,24 @@
+import { ChatDeepSeek } from "@langchain/deepseek";
+import dotenv from "dotenv";
+dotenv.config();
+
+const llm = new ChatDeepSeek({
+    model: "deepseek-reasoner",
+    temperature: 0,
+    apiKey: process.env.DEEPSEEK_API_KEY
+    // other params...
+});
+
+
+async function main() {
+    const aiMsg = await llm.invoke([
+        [
+            "system",
+            "You are a helpful assistant that translates English to French. Translate the user sentence.",
+        ],
+        ["human", "I love programming."],
+    ]);
+    console.log(aiMsg);
+}
+
+main();
